59d07bb716015b75188e241c2460cdf1a9be794a
Joshua Olds <joshua.olds@outlook.com>
2016-12-08 14:36:02 -0700

    Working on Vive stuffs

---
+++ Assets/Scripts/Painting/ViveControllerManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+public class ViveControllerManager : MonoBehaviour
+{
+    public enum Controller { LeftViveController, RightViveController};
+
+    public GameObject LeftViveControllerObject;
+    public GameObject RightViveControllerObject;
+    public bool LeftControllerEnabled; //Uncheck this in editor if not using this controller
+    public bool RightControllerEnabled;//Uncheck this in editor if not using this controller
+
+    private SteamVR_Controller.Device leftController;
+    private SteamVR_Controller.Device rightController;
+    private SteamVR_TrackedObject.EIndex leftControllerIndex;
+    private SteamVR_TrackedObject.EIndex rightControllerIndex;
+
+    public bool Ready; //Are both controllers found?
+
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    void Awake()
+    {
+        //Grab the controller indexes. This may happen too fast, so while loops...
+        leftControllerIndex = LeftViveControllerObject.GetComponent<SteamVR_TrackedObject>().index;
+        rightControllerIndex = RightViveControllerObject.GetComponent<SteamVR_TrackedObject>().index;
+
+        if (LeftControllerEnabled)
+        {
+            while((int)leftControllerIndex == -1)
+            {
+                leftControllerIndex = LeftViveControllerObject.GetComponent<SteamVR_TrackedObject>().index;
+            }
+        }
+        if (RightControllerEnabled)
+        {
+            while ((int)rightControllerIndex == -1)
+            {
+                rightControllerIndex = RightViveControllerObject.GetComponent<SteamVR_TrackedObject>().index;
+            }
+        }
+
+        Debug.Log("Left controller index: " + (int)leftControllerIndex);
+        Debug.Log("Right controller index: " + (int)rightControllerIndex);
+        leftController = SteamVR_Controller.Input((int)leftControllerIndex);
+        rightController = SteamVR_Controller.Input((int)rightControllerIndex);
+        Ready = true;
+    }
+
+    public bool isReady()
+    {
+        return Ready;
+    }
+
+    public bool getButtonDown(Valve.VR.EVRButtonId buttonId, ViveControllerManager.Controller controller)
+    {
+        if(controller == ViveControllerManager.Controller.LeftViveController && LeftControllerEnabled)
+        {
+            return leftController.GetPressDown(buttonId);
+        }
+        if(controller == ViveControllerManager.Controller.RightViveController && RightControllerEnabled)
+        {
+            return rightController.GetPressDown(buttonId);
+        }
+        return false;
+    }
+
+}
---
+++ Assets/Scripts/Painting/ViveControllerTester.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViveControllerTester : MonoBehaviour {
+
+    public ViveControllerManager ControllerManager;
+
+    public bool VerboseConsole; //Flag for verbose console logging. Not yet implemented
+    public bool RightControllerTriggerDown;
+    public bool LeftControllerTriggerDown;
+    public bool RightControllerGripDown;
+    public bool LeftControllerGripDown;
+    public bool RightControllerPadDown;
+    public bool LeftControllerPadDown;
+    public bool RightControllerPadUp;
+    public bool LeftControllerPadUp;
+    public bool RightControllerPadLeft;
+    public bool LeftControllerPadLeft;
+    public bool RightControllerPadRight;
+    public bool LeftControllerPadRight;
+
+    // Use this for initialization
+    void Start () {
+	    if(ControllerManager == null)
+        {
+            Debug.LogError("Missing Controller Manager! Will not be able to access Vive Controllers!");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!ControllerManager.isReady()) return; //Short circuit if not ready
+
+        LeftControllerTriggerDown = ControllerManager.getButtonDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger, ViveControllerManager.Controller.LeftViveController);
+        RightControllerTriggerDown = ControllerManager.getButtonDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger, ViveControllerManager.Controller.RightViveController);
+
+        LeftControllerGripDown = ControllerManager.getButtonDown(Valve.VR.EVRButtonId.k_EButton_Grip, ViveControllerManager.Controller.LeftViveController);
+        RightControllerGripDown = ControllerManager.getButtonDown(Valve.VR.EVRButtonId.k_EButton_Grip, ViveControllerManager.Controller.RightViveController);
+    }
+
+}
