40b3a4ecf33c4c35a866cb45760bae59f1f689cd
Joshua Olds <joshua.olds@outlook.com>
2016-12-12 17:40:21 -0700

    Git LFS Plugin end of day monday

---
+++ Assets/Scripts/Movement/Rotator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public float XRotationAmount;
+    public float YRotationAmount;
+    public float ZRotationAmount;
+    public float VariationAmount;
+
+    private float randomVariation;
+
+	// Use this for initialization
+	void Start () {
+        randomVariation = Random.Range(0.0f, VariationAmount);
+	}
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float x = XRotationAmount;
+        float y = YRotationAmount;
+        float z = ZRotationAmount;
+        if (XRotationAmount != 0.0f)
+        {
+            x += randomVariation;
+        }
+        if (YRotationAmount != 0.0f)
+        {
+            y += randomVariation;
+        }
+        if (ZRotationAmount != 0.0f)
+        {
+            z += randomVariation;
+        }
+        Vector3 eulers = new Vector3(x, y, z);
+        transform.Rotate(eulers);
+
+    }
+}
---
+++ Assets/Scripts/Vive/VivePickup.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VivePickup : MonoBehaviour {
+
+    //It is important to use an attachment transform that isn't scaled!!
+
+    public ViveControllerManager ControllerManager;
+    private Valve.VR.EVRButtonId GrabButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    public ViveControllerManager.Controller Controller;
+    public Transform AttachTransform;
+
+    private List<FixedJoint> attachedObjects = new List<FixedJoint>();
+
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// FixedUpdate is called once per physics frame
+	void FixedUpdate () {
+        if(ControllerManager.IsReady())
+        {
+            if (!ControllerManager.GetPress(GrabButton, Controller)) //Released button
+            {
+                foreach (FixedJoint fj in attachedObjects)
+                {
+                    attachedObjects.Remove(fj);
+                    Destroy(fj);
+                }
+            }
+        }
+	    
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(ControllerManager.IsReady())
+        {
+            if (ControllerManager.GetPress(GrabButton, Controller))
+            {
+
+                GameObject otherObject = other.gameObject;
+                if (otherObject.GetComponent<FixedJoint>() == null)
+                {
+                    FixedJoint fj = otherObject.AddComponent<FixedJoint>();
+                    Rigidbody rb = GetComponent<Rigidbody>();
+                    fj.connectedBody = rb;
+                }
+
+            }
+        }
+        
+    }
+}
