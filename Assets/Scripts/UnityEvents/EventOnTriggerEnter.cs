1c14a99a1d4876bba44c2df95e04482d043119e2
Josh Olds <joshua.olds@outlook.com>
2016-12-16 11:20:01 -0700

    Xylophone implemented

---
+++ Assets/Scripts/UnityEvents/EventOnTriggerEnter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class EventOnTriggerEnter : MonoBehaviour {
+
+    public UnityEvent OnTriggerEnterEvent;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        OnTriggerEnterEvent.Invoke();
+    }
+}
